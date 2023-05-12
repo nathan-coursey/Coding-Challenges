bd406c5d66b14ee0c71e1850cb230b48cabcf7af
nathan-coursey <37522514+nathan-coursey@users.noreply.github.com>
2023-05-12 17:43:21 -0500

    Update Week3_Challenge.js

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,10 @@
+function FirstReverse(str) { 
 
+  // code goes here
+  str = str.split('').reverse().join('')
+  return str; 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
