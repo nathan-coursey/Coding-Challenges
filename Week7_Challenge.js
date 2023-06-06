a840d712b3b7a0a614773073700fdeeb49ee9ab4
nathan-coursey <37522514+nathan-coursey@users.noreply.github.com>
2023-06-06 18:13:48 -0500

    Update Week7_Challenge.js

--- Week7_Challenge.js
+++ Week7_Challenge.js
@@ -1,1 +1,18 @@
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
 
+	compareAge(other) {
+		let compare;
+		if (this.age > other.age) {
+			compare = "younger than";
+		} else if (this.age < other.age) {
+			compare = "older than";
+		} else {
+			compare = "the same age as";
+		}
+		return `${other.name} is ${compare} me.`;
+	}
+}
