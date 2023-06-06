99ab54090c02898bfe32b88fcb1062652ef5cb11
nathan-coursey <37522514+nathan-coursey@users.noreply.github.com>
2023-06-06 18:08:32 -0500

    Update Week4_Challenge.js

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,12 @@
+function LongestWord(sen) { 
+  let words = sen.match(/\w+/g);
+  let longest = words[0];
+  for (let i=0; i<words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i]
+    }
+  }
+  return longest;
+}
 
+console.log(LongestWord(readline()));
